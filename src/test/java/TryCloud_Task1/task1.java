4a312a39745da9dfaf881f43cb3ecf6187f1a8cb
Baris312 <barmagan81@gmail.com>
2021-02-01 20:54:20 -0600

    task1

---
+++ src/test/java/TryCloud_Task1/task1.java
@@ -0,0 +1,9 @@
+package TryCloud_Task1;
+
+public class task1
+    {
+        public static void main(String[] args)
+            {
+                System.out.println("TryCloud  starts 2/1/21 ");
+            }
+    }
