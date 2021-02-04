1126956aae7ac465268430cf5addb5e4d7bdd8c8
Baris312 <barmagan81@gmail.com>
2021-02-04 14:45:29 -0600

    trylcoud login updated

---
+++ src/test/java/Baris/WebElementLoc.java
@@ -0,0 +1,30 @@
+package Baris;
+
+import Utilities.Driver;
+import Utilities.Login;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static Utilities.Login.LoginSetup;
+
+public class WebElementLoc
+    {
+
+
+        @Test
+     public void loc()
+            {
+                WebDriver driver = Driver.getDriver();
+                LoginSetup(driver);
+
+                WebElement file= driver.findElement(By.xpath("//ul[@id= 'appmenu']//li[2]"));
+
+                Assert.assertTrue(file.isDisplayed());
+            }
+    }
+
+
+////ul[@id= 'appmenu']//li[2]
