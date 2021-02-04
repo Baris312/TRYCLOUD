7021acf606ca9ab8599778be0e372b4f12ff2df3
CassandraBd <bodencassandra@gmail.com>
2021-02-04 09:46:50 -0600

    Second commit today

---
+++ src/test/java/Cassandra/ModulesTask.java
@@ -0,0 +1,60 @@
+package Cassandra;
+
+import Utilities.Driver;
+import Utilities.Wait;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static Utilities.Login.LoginSetup;
+
+public class ModulesTask {
+
+    // 1.Login as a user
+    // 2.Verify the user see the following modules: Files, Galleries, Activity, Talk, Contacts, Calendar, Note
+
+    @Test
+    public void mainModules(){
+
+        WebDriver driver = Driver.getDriver();
+        LoginSetup(driver);
+        Wait.Wait(3);
+
+        WebElement files = driver.findElement(By.id("files"));
+        WebElement activity = driver.findElement(By.id("activities"));
+        WebElement gallery = driver.findElement(By.id("gallery"));
+        WebElement talk = driver.findElement(By.id("spreed"));
+        WebElement calendar = driver.findElement(By.id("calendar"));
+        WebElement deck = driver.findElement(By.id("deck"));
+        WebElement announcements = driver.findElement(By.id("announcementcenter"));
+
+       ArrayList<String> modules = new ArrayList<String>(Arrays.asList("files,activity, gallery, talk, calendar, deck, announcements"));
+
+        String isDisplayed = "";
+       for (String each : modules) {
+         //  Assert.assertTrue(each.isDisplayed());
+
+       }
+
+
+
+          //  WebElement files = Driver.getDriver().findElement(By.xpath("//select[@id='files']));
+          //  WebElement files = Driver.getDriver().findElement(By.xpath("//select[@id="appmenu"]/li[1]]));
+
+
+
+         //   String expectedModule = "You can see modules";
+          //  Assert.assertTrue(resultText.isDisplayed());
+          //  Assert.assertEquals(actualModule, expectedModule);
+
+        }
+
+
+    }
+
+
