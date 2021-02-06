690c7570e3a45926e38eeb0fdb6eaa7ddb4a5874
Baris312 <barmagan81@gmail.com>
2021-02-05 23:43:58 -0600

    4

--- src/test/java/TRYCLOUD/MAIN_Class.java
+++ src/test/java/TRYCLOUD/MAIN_Class.java
@@ -1,6 +1,7 @@
 package TRYCLOUD;
 
 import Utilities.Driver;
+import Utilities.Login;
 import Utilities.Wait;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -139,7 +140,7 @@ public static  void  UserLogin()
 
             }
 
-
+        // Test case3_3 && TestCase3_4
         @Test
         public void TestCase3_3()
             {
@@ -170,7 +171,7 @@ public static  void  UserLogin()
 
 
             }
-            // 2.Click action-icon from any file on the page
+            // Test case3_3 && TestCase3_4
              public void runTest3()
                  {
                      WebDriver driver = Driver.getDriver();
@@ -324,6 +325,45 @@ public static  void  UserLogin()
 
 
         }
+
+            @Test
+           public void TestCase4()
+            {
+
+                // Test case #2 - verify users can send message
+                // 1 Login as a user
+
+                WebDriver driver = Driver.getDriver();
+
+
+
+
+                // 2. Click Talks module
+                WebElement talks = Driver.getDriver().findElement(By.xpath("//li[@data-id='spreed'][1]"));
+                talks.click();
+                //3. Search a user from search box on the left
+                Driver.getDriver().findElement(By.xpath("//input[@type='text']")).sendKeys("marufjon");
+                WebElement user = Driver.getDriver().findElement(By.xpath("//span[@class='acli__content__line-one__title']"));
+                user.click();
+                //4. Write a message
+                WebElement message = Driver.getDriver().findElement(By.xpath("//div[@role='textbox']"));
+                message.sendKeys("Hello Muhtar");
+
+                //5. Click submit button
+                WebElement submit = Driver.getDriver().findElement(By.xpath("//button[@type='submit']"));
+                submit.click();
+
+                //6. Verify the message is displayed on the conversation log
+                String actualResult = Driver.getDriver().findElement(By.xpath("(//div[@class='messages'])[2]")).getText();
+                String expectedResult = "Hello Muhtar";
+                Assert.assertTrue(actualResult.contains(expectedResult));
+                System.out.println(actualResult);
+
+                Wait.Wait(2);
+                driver.get("http://qa3.trycloud.net/index.php/apps/dashboard/");
+
+            }
+
 
 
 
